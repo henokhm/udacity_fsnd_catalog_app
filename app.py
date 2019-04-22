baaf6916474bbaaf4822e5cdb5065a886c054aac
Henok Hailemariam <henokhm2@gmail.com>
2019-04-22 18:10:40 +0300

    fixed conflicting routes

--- app.py
+++ app.py
@@ -24,59 +24,59 @@ def logout():
 
 
 # category CRUD
-@app.route("/add_category")
+@app.route("/catalog_categories/add_category")
 def add_category():
     return "Add New Category Page"
 
 
-@app.route("/catalog/<string:category_name>/items")
+@app.route("/catalog_categories/<string:category_name>/items")
 def show_category(category_name):
     return "Display Page for {} Items".format(category_name)
 
 
-@app.route("/catalog/<string:category_name>/edit")
+@app.route("/catalog_categories/<string:category_name>/edit")
 def edit_category(category_name):
     return "Edit Category Page for {}".format(category_name)
 
 
-@app.route("/catalog/<string:category_name>/delete")
+@app.route("/catalog_categories/<string:category_name>/delete")
 def delete_category(category_name):
     return "Delete Category Page for {}".format(category_name)
 
 
 # items CRUD
-@app.route("/catalog/add_item")
+@app.route("/catalog_items/add_item")
 def add_item():
     return "Add new Item Page"
 
 
-@app.route("/catalog/<string:item_name>")
-def show_item_details(category_name, item_name):
-    return "Item details Page for item:{} in category:{}".format(item_name, category_name)
+@app.route("/catalog_items/<string:item_name>")
+def show_item_details(item_name):
+    return "Item details Page for item:{}".format(item_name)
 
 
-@app.route("/catalog/<string:item_name>/edit")
+@app.route("/catalog_items/<string:item_name>/edit")
 def edit_item_details(item_name):
     return "Edit Item details Page for item:{}".format(item_name)
 
 
-@app.route("/catalog/<string:item_name>/delete")
+@app.route("/catalog_items/<string:item_name>/delete")
 def delete_item_details(item_name):
-    return "Edit Item details Page for item:{}".format(item_name)
+    return "Delete Item details Page for item:{}".format(item_name)
 
 
-@app.route("/catalog/json")
+@app.route("/catalog.json")
 def full_catalog_json():
     return "Full catalog JSON"
 
 
-@app.route("/<string:category_name>.json")
-def full_category_json():
+@app.route("/category/<string:category_name>.json")
+def full_category_json(category_name):
     return "Full Category JSON"
 
 
-@app.route("/<string:category_name>/<string:item_name>.json")
-def single_item_json():
+@app.route("/item/<string:item_name>.json")
+def single_item_json(item_name):
     return "Single Item JSON"
 
 
